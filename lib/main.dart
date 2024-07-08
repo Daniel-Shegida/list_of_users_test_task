3d6f8b4300ce11492b3337fa6aaa417246203d60
daniel shegida <den.shegida@gmail.com>
2024-07-08 12:15:00 +0300

    feat(apk): added env and main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:list_of_users_test_task/di/locator.dart';
 import 'package:list_of_users_test_task/presentation/pages/users_search/users_search_page.dart';
 
-void main() {
+void main() async{
+  await dotenv.load(fileName: ".env");
+  initDependencies();
   runApp(const MyApp());
 }
 
